2978f0e9c6f9816b8348b2e7fea855e72620e9ae
hmasoud <hudamasoud86@gmail.com>
2020-02-23 21:47:49 +0400

    Add search functionality

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -8,10 +8,69 @@ import 'package:alnaiem/util/categoy_utils.dart';
 CategoryUtil athkars = new CategoryUtil();
 List<AthkarCategory> categories;
 
-class HomePage extends StatelessWidget {
+List<AthkarCategory> _athkarCategoryList = [];
+List<AthkarCategory> _filteredList = [];
+String filter = "";
+TextEditingController controller;
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    controller = new TextEditingController();
+    filter = "";
+    categories = athkars.getCategories();
+    List<AthkarCategory> tmpList = new List<AthkarCategory>();
+    for (int i = 0; i < categories.length; i++) {
+      tmpList.add(categories[i]);
+    }
+    setState(() {
+      _athkarCategoryList = tmpList;
+      _filteredList = _athkarCategoryList;
+      controller.addListener(() {
+        if (controller.text.isEmpty) {
+          setState(() {
+            filter = "";
+            _filteredList = _athkarCategoryList;
+          });
+        } else {
+          setState(() {
+            filter = controller.text;
+          });
+        }
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    categories = athkars.getCategories();
+    setState(
+      () {
+        if ((filter.isNotEmpty)) {
+          List<AthkarCategory> tmpList = new List<AthkarCategory>();
+          for (int i = 0; i < _athkarCategoryList.length; i++) {
+            if (_athkarCategoryList[i]
+                .title
+                .toLowerCase()
+                .contains(filter.toLowerCase())) {
+              tmpList.add(_athkarCategoryList[i]);
+            }
+          }
+          _filteredList = tmpList;
+        }
+      },
+    );
 
     return Stack(
       children: <Widget>[
@@ -24,38 +83,85 @@ class HomePage extends StatelessWidget {
         Scaffold(
           backgroundColor: Colors.transparent,
           appBar: AppBar(
-              title: Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                kHomePageHeaderText,
-                style: kHeaderTextStyle,
-              ),
-            ],
-          )),
-          body: ListView(
-            children: <Widget>[
-              for (var i = 0; i < categories.length; i++)
-                ReusableButton(
-                  buttonTitle: categories[i].title,
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                        builder: (context) => SubCategory(
-                          subCategoryTitle: categories[i].title,
-                          currentIndex: i,
-                          athkarCategory: categories[i],
+            title: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  kHomePageHeaderText,
+                  style: kHeaderTextStyle,
+                ),
+              ],
+            ),
+          ),
+          body: Container(
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Container(
+                    height: 60.0,
+                    child: TextField(
+                      autocorrect: false,
+                      style: new TextStyle(color: Colors.black),
+                      onChanged: (value) {},
+                      controller: controller,
+                      decoration: InputDecoration(
+                        hintText: "بحث",
+                        hintStyle: TextStyle(
+                            fontSize: 20.0, color: kButtonBorderColor),
+                        fillColor: Colors.white.withOpacity(0.5),
+                        filled: true,
+                        prefixIcon: Icon(
+                          Icons.search,
+                          color: kButtonBorderColor,
+                        ),
+                        border: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            //color: kButtonBorderColor,
+                            color: Colors.blue,
+                            style: BorderStyle.solid,
+                          ),
+                          borderRadius: BorderRadius.all(
+                            Radius.circular(25.0),
+                          ),
                         ),
                       ),
-                    );
-                  },
-                  color: (i % 2) == 0 ? kLightButtonColor : kDarkButtonColor,
-                  imagePath: categories[i].imagePath,
+                    ),
+                  ),
                 ),
-            ],
+                Expanded(
+                  child: ListView.builder(
+                    scrollDirection: Axis.vertical,
+                    shrinkWrap: true,
+                    itemCount:
+                        _athkarCategoryList == null ? 0 : _filteredList.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return ReusableButton(
+                        buttonTitle: _filteredList[index].title,
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => SubCategory(
+                                subCategoryTitle: _filteredList[index].title,
+                                currentIndex: index,
+                                athkarCategory: _filteredList[index],
+                              ),
+                            ),
+                          );
+                        },
+                        color: (index % 2) == 0
+                            ? kLightButtonColor
+                            : kDarkButtonColor,
+                        imagePath: _filteredList[index].imagePath,
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
           ),
-        )
+        ),
       ],
     );
   }
