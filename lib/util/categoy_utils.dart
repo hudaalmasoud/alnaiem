de03f880a9249039e61d25fdf969044960ff941c
hmasoud <hudamasoud86@gmail.com>
2020-03-07 21:27:57 +0400

    - fix athkar text

--- lib/util/categoy_utils.dart
+++ lib/util/categoy_utils.dart
@@ -1110,7 +1110,7 @@ class CategoryUtil {
     List<SubCategoryDetails> backToHomeAthkars = new List<SubCategoryDetails>();
     backToHomeAthkars.add(
       new SubCategoryDetails(
-          'َبِسْمِ اللَّهِ وَلَجْنَا، و بِسْمِ اللَّهِ خَرَجْنَا، وَعَلَى رَبِّنَا تَوَكَّلْنَا، ثُمَّ لِيُسَلِّمْ عَلَى أَهْلِه.',
+          'بِسْمِ اللَّهِ وَلَجْنَا، وبِسْمِ اللَّهِ خَرَجْنَا، وَعَلَى رَبِّنَا تَوَكَّلْنَا، ثُمَّ لِيُسَلِّمْ عَلَى أَهله.',
           1,
           0),
     );
